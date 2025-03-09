a8c370ccf4a215b154df3e20d4a22657b602cecf
Devin Schutz <schutzdevin@gmail.com>
2025-03-08 22:20:39 -0700

    got rid of unused import

--- app/forms/forms.py
+++ app/forms/forms.py
@@ -1,6 +1,6 @@
 from wtforms import Form, StringField, PasswordField, validators, FileField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
-from flask_wtf.file import FileRequired, FileAllowed
+from flask_wtf.file import FileAllowed
 
 # validators can have the form have specific requirements about lengths and stuff like that\
 
@@ -8,16 +8,15 @@ class LoginForm(Form):
     username = StringField('Name', [validators.length(min=4, max=25)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password')
-    submit = SubmitField('Register')
+    submit = SubmitField('Login')
 
 class RegistrationForm(Form):
     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', [
         validators.DataRequired(), # make sure they include this
-        validators.EqualTo('confirm', message="Passwords must match") # make sure this matches the confirm field
+        validators.EqualTo('confirm_password', message="Passwords must match") # make sure this matches the confirm field
     ])
-    confirm = PasswordField('Repeat Password')
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
     submit = SubmitField('Register')
 
